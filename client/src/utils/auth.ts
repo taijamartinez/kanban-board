afbc69093ed8e564a3556f26dea36d2a31867cb5
taijamartinez <taijasmartinez@gmail.com>
2025-02-23 15:10:50 -0500

    added code to server auth.ts and client auth.ts

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -3,28 +3,41 @@ import { JwtPayload, jwtDecode } from 'jwt-decode';
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
+    const token = this.getToken();
+    return jwtDecode(token);
   }
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return token;
   }
   
   isTokenExpired(token: string) {
     // TODO: return a value that indicates if the token is expired
+    const decoded: JwtPayload = jwtDecode(token);
+    const currentTime = Date.now() / 1000;
+    return decoded.exp ? decoded.exp < currentTime : false;
   }
 
   getToken(): string {
     // TODO: return the token
+    const loggedUser = localStorage.getItem('id_token') || '';
+    return loggedUser;
   }
 
   login(idToken: string) {
     // TODO: set the token to localStorage
     // TODO: redirect to the home page
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
   }
 
   logout() {
     // TODO: remove the token from localStorage
     // TODO: redirect to the login page
+      localStorage.removeItem('id_token');
+      window.location.assign('/');
   }
 }
 
