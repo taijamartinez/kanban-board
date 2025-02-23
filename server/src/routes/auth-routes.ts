0b4f13a50269d578d202051add8f1f46303479ce
taijamartinez <taijasmartinez@gmail.com>
2025-02-23 12:22:56 -0500

    added .gitignore to the root and added to-do code in auth-routes.ts file

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -3,9 +3,33 @@ import { User } from '../models/user.js';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 
+import dotenv from 'dotenv'; //added lines 6&7. import .env file
+dotenv.config();
+
+//added lines 11-30
 export const login = async (req: Request, res: Response) => {
   // TODO: If the user exists and the password is correct, return a JWT token
-};
+  const { username, password } = req.body;
+
+  const user = await User.findOne({
+    where: { username },
+  });
+
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+  const passwordIsValid = await bcrypt.compare(password, user.password);
+  if (!passwordIsValid) {
+    return res.status(401).json({ message: 'Invalid password' });
+  }
+
+  const secretKey = process.env.JWT_SECRET_KEY || '';
+
+  const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
+
+  res.json({ token });
+}
 
 const router = Router();
 
